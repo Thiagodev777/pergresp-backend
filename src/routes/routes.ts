ebccdccc8bb94363623a1603d1776ff3af1d49d7
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-12 21:15:39 -0300

    database settings

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,13 @@
 import { Router, Response, Request } from 'express'
+import { sequelize } from '../config/database-mysql/mysql'
 
 const router = Router()
+
+try {
+  sequelize.authenticate().then(() => console.log('connect with success'))
+} catch (err) {
+  console.log(err)
+}
 
 router.get('/', (req: Request, res: Response) => {
   res.send('ok')
